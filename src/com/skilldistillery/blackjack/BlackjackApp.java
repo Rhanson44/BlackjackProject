c32a31038b54df9404cd432f122d6bac9b5d9140
Ryan Hanson <hansonrj8322@gmail.com>
2024-07-13 13:43:07 -0600

    App refactor

--- src/com/skilldistillery/blackjack/BlackjackApp.java
+++ src/com/skilldistillery/blackjack/BlackjackApp.java
@@ -5,75 +5,101 @@ import java.util.Scanner;
 public class BlackjackApp {
 	private Dealer dealer;
 	private Player player;
+	private Scanner scan;
 
 	public static void main(String[] args) {
 		BlackjackApp app = new BlackjackApp();
 		app.run();
 	}
-
-	private void run() {
-		Scanner scan = new Scanner(System.in);
-		int turn = 0;
+	
+	public BlackjackApp() {
 		this.dealer = new Dealer();
 		this.player = new Player();
-		if (turn == 0) {
+		this.scan = new Scanner(System.in);
+	}
+
+	private void run() {
+		
+		firstHand();
+		
+		playerTurn();
+		
+		dealerTurn();
+		
+		checkWins();
+		
+	}
+	
+	private void firstHand() {
+		while(true) {
 			player.drawBlackjackCard(dealer.dealCard());
 			System.out.println(player.displayHand());
+			
 			dealer.drawBlackjackCard(dealer.dealCard());
 			System.out.println("Dealer hand: [Hidden Card]");
 			player.drawBlackjackCard(dealer.dealCard());
-			if(player.getBlackjackHandValue() == 21) {
+			
+			if(player.isBlackjack()) {
 				System.out.println(player.displayHand());
 				System.out.println("Blackjack!");
-				System.exit(0);
+				break;
 			}
 			System.out.println(player.displayHand());
 			dealer.drawBlackjackCard(dealer.dealCard());
 			System.out.println(dealer.displayHand());
-			turn++;
+			break;
 		}
+	}
+	
+	private void playerTurn() {
+		boolean isTrue = true;
+		
 		System.out.println("1 - Hit  :  2 - Stay");
 		int choice = scan.nextInt();
-		while (choice == 1) {
-			player.drawBlackjackCard(dealer.dealCard());
-			System.out.println(player.displayHand());
-			if (player.getBlackjackHandValue() == 21) {
-				System.out.println("Blackjack!");
-				choice = -1;
-			} else if (player.getBlackjackHandValue() > 21) {
-				System.out.println("Bust!");
-				choice = 0;
-			} else {
-				System.out.println("1 - Hit  :  2 - Stay");
-				choice = scan.nextInt();
-			}
-		}
-		while (choice == 0 || choice == 2) {
-			if (dealer.getBlackjackHandValue() >= 17) {
-				System.out.println("Dealer stays");
-				System.out.println(dealer.displayHiddenHand());
-			}
-			while (dealer.getBlackjackHandValue() <= 17) {
-				dealer.drawBlackjackCard(dealer.dealCard());
-				if (dealer.getBlackjackHandValue() >= player.getBlackjackHandValue() && dealer.getBlackjackHandValue() <= 21) {
-					System.out.println("Dealer wins");
-				} else if (dealer.getBlackjackHandValue() == 21) {
-					System.out.println(dealer.displayHiddenHand());
-					System.out.println("Dealer blackjack!");
-					choice = -1;
-				} else if (dealer.getBlackjackHandValue() > 21) {
-					System.out.println(dealer.displayHiddenHand());
-					System.out.println("Dealer bust");
-					if (player.getBlackjackHandValue() <= 21) {
-						System.out.println("You win!");
-					} else if (player.getBlackjackHandValue() > 21) {
-						System.out.println("No one wins");
-					}
-					choice = -1;
+		
+		while(isTrue) {
+			if (choice == 2) {
+				isTrue = false;
+			} else if (choice == 1) {
+				player.drawBlackjackCard(dealer.dealCard());
+				System.out.println(player.displayHand());
+				
+				if (player.isBlackjack()) {
+					System.out.println("Blackjack!");
+					System.exit(0);
+				} else if (player.isBust()) {
+					System.out.println("Bust!");
+					isTrue = false;
+				} else {
+					System.out.println("1 - Hit  :  2 - Stay");
+					choice = scan.nextInt();
 				}
 			}
-			
-			scan.close();
+		}
+	}
+	
+	private void dealerTurn() {
+		
+		System.out.println(dealer.displayHiddenHand());
+		
+		if (dealer.getBlackjackHandValue() >= 17) {
+			System.out.println("Dealer stays");
+		}
+		
+		while (dealer.getBlackjackHandValue() < 17) {
+			dealer.drawBlackjackCard(dealer.dealCard());
+			System.out.println(dealer.displayHiddenHand());
+		}
+	}
+	
+	private void checkWins() {
+		
+		if(player.getBlackjackHandValue() > dealer.getBlackjackHandValue() && !player.isBust()) {
+			System.out.println("Player wins!");
+		} else if (player.isBust() && dealer.isBust()) {
+			System.out.println("No one wins");
+		} else if (!dealer.isBust() && dealer.getBlackjackHandValue() > player.getBlackjackHandValue() || dealer.isBlackjack()) {
+			System.out.println("Dealer wins");
 		}
 	}
 }
